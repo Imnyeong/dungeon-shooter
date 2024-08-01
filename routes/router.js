0c0de223d05213a96eb8ffebf6e06408e704b85d
Imnyeong <lmh4873@gmail.com>
2024-08-01 15:37:50 +0900

    feat: 로그인 구현

--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 var sql = require('../sql');
 
-router.get("/getuserlist", function(req, res, next) {
-  sql.getUsers(req, res, next);
+router.get("/getuserlist", function(req, res) {
+  sql.getUserList(req, res);
 });
 
-router.post("/register", function(req, res, next) {
-  sql.addUser(req, res, next);
+router.get("/getuserinfo/:id", function(req, res) {
+  sql.getUserInfo(req.params, res);
+});
+
+router.post("/register", function(req, res) {
+  sql.registerUser(req, res);
+});
+
+router.get("/login/:user", function(req, res) {
+  sql.login(req.params, res);
 });
 
 module.exports = router;
--- sql.js
+++ sql.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var action = require('./action');
 var db = require('./db');
+const { setDefaultResultOrder } = require('dns');
 
 var con = db.init();
 
@@ -8,18 +9,14 @@ var userTable = 'Users';
 
 module.exports = 
 {
-    getUsers : function(req, res, next)
+    getUserList : function(req, res)
     {
         var query = util.format("SELECT * FROM %s", userTable);
 
-        con.query(query, function (error, results, fields)
+        con.query(query, function (error, results)
         {
             if(error)
-            {
                 throw error;
-            }
-            
-            action.showUsers(results);
 
             return res.send(JSON.stringify({
                 code : 200,
@@ -28,24 +25,65 @@ module.exports =
         });
     },
 
-    addUser : function(req, res, next)
+    getUserInfo : function(req, res)
+    {
+        var query = util.format("SELECT * FROM %s WHERE ID = '%s'", userTable, req.id);
+
+        con.query(query, function (error, results)
+        {
+            if(error)
+                throw error;
+
+            return res.send(JSON.stringify({
+                code : 200,
+                message : JSON.stringify(results)
+            }));
+        });       
+    },
+
+    registerUser : function(req, res)
     {
         var query = util.format("INSERT INTO %s(ID, PW, Nickname) Values (?, ?, ?);", userTable);
 
         var data = req.body[0];
         var values = [data.ID, data.PW, data.Nickname];
         
-        con.query(query, values, function(error, results, fields)
+        con.query(query, values, function(error)
           {
             if(error)
-              {
                   throw error;
-              }  
 
               return res.send(JSON.stringify({
                 code : 200,
                 message : "Register Success"
               }));
+          });
+    },
+
+    login : function(req, res)
+    {
+        var user = JSON.parse(req.user);
+        var query = util.format("SELECT * FROM %s WHERE ID = '%s' AND PW = '%s'", userTable, user.ID, user.PW);
+        
+        con.query(query, function(error, results)
+          {
+            if(error)
+                  throw error;
+
+            if(results[0] == null)
+            {
+                return res.send(JSON.stringify({
+                    code : 200,
+                    message : 'Login Failed'
+                }));
+            }
+            else
+            {
+                return res.send(JSON.stringify({
+                    code : 200,
+                    message : JSON.stringify(results[0])
+                }));
+            }
           });
     }
 }
