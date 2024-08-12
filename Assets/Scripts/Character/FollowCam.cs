f048d6f0d45d86845eab81335659989e0e4faaa9
Imnyeong <lmh4873@gmail.com>
2024-08-12 16:04:14 +0900

    feat: Camera가 벽을 뚫지 않는 기능 구현

--- Assets/Scripts/Character/FollowCam.cs
+++ Assets/Scripts/Character/FollowCam.cs
@@ -17,6 +17,8 @@ namespace DungeonShooter
         public float camDistance = 3.0f;
         public float camHeight = 3.0f;
 
+        [SerializeField] private LayerMask wall;
+
         public void SetTarget(int _id)
         {
             player = GameManager.instance.players.Find(x => x.id == _id).gameObject.transform;
@@ -39,8 +41,18 @@ namespace DungeonShooter
         {
             transform.eulerAngles = new Vector3(mouseY, mouseX, transform.rotation.z);
             player.localRotation = Quaternion.Euler(0, mouseX, 0);
-            transform.position =
-                new Vector3(player.position.x, player.position.y + camHeight, player.position.z) - transform.forward * camDistance;
+
+            Vector3 rayDirection = (transform.position - player.position).normalized;
+            
+            if (Physics.Raycast(player.position, rayDirection, out RaycastHit hit, float.MaxValue, wall))
+            {
+                float distance = Mathf.Min(Vector3.Distance(player.position, hit.point), camDistance);
+                transform.position = new Vector3(player.position.x, player.position.y + camHeight, player.position.z) - transform.forward * distance;
+            }
+            else
+            {
+                transform.position = new Vector3(player.position.x, player.position.y + camHeight, player.position.z) - transform.forward * camDistance;
+            }
         }
     }
 }
