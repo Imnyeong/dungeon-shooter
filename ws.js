710014cc5a523cfc2c8034726fca082b66aa561a
Imnyeong <lmh4873@gmail.com>
2024-08-14 13:29:30 +0900

    feat: 방 나누기

--- ws.js
+++ ws.js
@@ -2,26 +2,40 @@ var WebSocket = require('ws');
 
 var wss;
 
-wss.on('connection', (ws) =>
+wss.on('connection', (ws, req) =>
 {
-    console.log("New Client Connected");
-
-    ws.on('message', (message) => 
+    console.log(req);
+    console.log('Connected');
+    if(req.url.startsWith(''))
     {
-        console.log(`Receiced message: ${message}`);
-        var packet = JSON.parse(message);
-        console.log(`Receiced packetType: ${packet.packetType}`);
-        console.log(`Receiced packetdata: ${packet.data}`);
-
-        wss.clients.forEach(function each(client) {
-            // 모든 클라이언트에 전송
-            client.send(message.toString());
-            if(client != ws)
+        //console.log('Connected With Room');
+        ws.location = req.url;
+        //console.log("ws.location = " + ws.location);
+        ws.on('message', (message) => 
             {
-                // 자신을 제외한 클라이언트에 전송
-            }
-        });
-    });
+                wss.clients.forEach(function each(client)
+                {
+                    //if(client != ws)
+                    if(client === ws.location)
+                    {
+                        client.send(message.toString());
+                    }
+                        // 모든 클라이언트에 전송
+                        //if(client != ws)
+                        //{
+                            // 자신을 제외한 클라이언트에 전송
+                        //}
+                });
+                //console.log(`Receiced message: ${message}`);
+                //var packet = JSON.parse(message);
+                //console.log(`Receiced packetType: ${packet.packetType}`);
+                //console.log(`Receiced packetdata: ${packet.data}`);
+            });
+    }
+    else
+    {
+        console.log('Connected Without Room');
+    }
 
     ws.on('close', () => 
     {
