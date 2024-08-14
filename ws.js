6fd530c5cf318a634224ff0675878c23d26f9a06
Imnyeong <lmh4873@gmail.com>
2024-08-14 15:30:54 +0900

    feat: 방 기능 구체화

--- ws.js
+++ ws.js
@@ -4,39 +4,26 @@ var wss;
 
 wss.on('connection', (ws, req) =>
 {
-    console.log(req);
-    console.log('Connected');
     if(req.url.startsWith(''))
     {
-        //console.log('Connected With Room');
         ws.location = req.url;
-        //console.log("ws.location = " + ws.location);
+        
         ws.on('message', (message) => 
             {
                 wss.clients.forEach(function each(client)
                 {
-                    //if(client != ws)
-                    if(client === ws.location)
+                    if(client != ws && client.location === ws.location)
                     {
+                        //var packet = JSON.parse(message);
                         client.send(message.toString());
                     }
-                        // 모든 클라이언트에 전송
-                        //if(client != ws)
-                        //{
-                            // 자신을 제외한 클라이언트에 전송
-                        //}
                 });
-                //console.log(`Receiced message: ${message}`);
-                //var packet = JSON.parse(message);
-                //console.log(`Receiced packetType: ${packet.packetType}`);
-                //console.log(`Receiced packetdata: ${packet.data}`);
             });
     }
     else
     {
-        console.log('Connected Without Room');
+        console.log('Wrong Connection');
     }
-
     ws.on('close', () => 
     {
         console.log('Client Disconnected');
