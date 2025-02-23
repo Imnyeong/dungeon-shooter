b2d8d46f6a06fd12b31ff80bc91c9f9faad48af1
이민형 <>
2025-02-23 19:33:05 +0900

    feat: REST 규칙 적용

--- routes/router.js
+++ routes/router.js
@@ -3,45 +3,45 @@ var router = express.Router();
 var sql = require('../sql');
 
 //#region User
-router.get("/getuserlist", function(req, res) {
+router.get("/users", function(req, res) {
   sql.getUserList(req, res);
 });
 
-router.get("/getuserinfo/:id", function(req, res) {
+router.get("/users/:id", function(req, res) {
   sql.getUserInfo(req.params, res);
 });
 
-router.post("/register", function(req, res) {
+router.post("/users", function(req, res) {
   sql.registerUser(req, res);
 });
 
-router.get("/login/:user", function(req, res) {
+router.get("/users/:user", function(req, res) {
   sql.login(req.params, res);
 });
 //#endregion
 
 //#region Room
-router.get("/getroomlist", function(req, res) {
+router.get("/rooms", function(req, res) {
   sql.getRoomList(req, res);
 });
 
-router.post("/createroom", function(req, res) {
+router.post("/rooms", function(req, res) {
   sql.createRoom(req, res);
 });
 
-router.post("/modifyroom", function(req, res) {
-  sql.modifyRoom(req, res);
-});
+//router.post("/modifyroom", function(req, res) {
+//  sql.modifyRoom(req, res);
+//});
 
-router.post("/deleteroom", function(req, res) {
+router.delete("/rooms", function(req, res) {
   sql.deleteRoom(req, res);
 });
 
-router.get("/getroominfo/:id", function(req, res) {
+router.get("/rooms/:id", function(req, res) {
   sql.getRoomInfo(req.params, res);
 });
 
-router.get("/getroommember/:id", function(req, res) {
+router.get("/members/:id", function(req, res) {
   sql.getRoomMeber(req.params, res);
 });
 //#endregion
